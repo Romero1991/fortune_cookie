4e632acfea32973ad2159a8ddda997a8d9d93b8b
MONTSERRAT MORALES <MONTSERRAT MORALES>
2016-10-25 08:36:37 -0500

    servidor html basico

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var colors = require('colors');
+//var colors = require('colors');
 
 //cargando configuraciones
 var config = require('./config/config');
@@ -7,13 +7,16 @@ var IP = 'config.IP';
 var PORT = 'config.PORT';
 
 // tema de colors
-colors.setTheme(config.color_theme);
+//colors.setTheme(config.color_theme);
 
 var server = http.createServer(function(req, res){// codigo de nuestro server
 
     res.writeHead(200,{
-        'Content-Type':'text/plain'
+        'Content-Type':'text/html'
     });
+res.write('<h1> mi sistema asombroso</h1>');
+res.write(`<script> var saludo = function(){alert("hola");}</script>`);
+res.write('<button onclick="saludo()"> SALUDAR</button>');
 
     res.end('> Server Funcionando');
 });
