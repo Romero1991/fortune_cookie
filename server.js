bc706989a19e0c87f2596e1caabc414b61db5d11
MONTSERRAT MORALES <MONTSERRAT MORALES>
2016-10-25 08:25:00 -0500

    se crea server de texto plano

---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var server = http.createServer(function(req, res){// codigo de nuestro server
+
+    res.writeHead(200,{
+        'Content-Type':'text/plain'
+    });
+
+    res.end('> Server Funcionando');
+});
+
+server.listen(3000,'127.0.0.1',function(){
+    console.log(`> Server working @http://127.0.0.1:3000/`);
+});
