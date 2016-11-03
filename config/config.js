1ac668ca1c2324fded5e32bad166b1776bad429b
Eduardo <er96099@gmail.com>
2016-11-03 07:56:11 -0600

    prueba

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-module.exports = {"IP": process.env.IP || '127.0.0.1', "PORT": process.env.PORT || 3030,
+module.exports = {"IP": process.env.IP || '127.0.0.1', "PORT": process.env.PORT || 3000,
  "colors_Theme":{
     
   "info":"rainbow",
--- server.js
+++ server.js
@@ -1,37 +1,31 @@
 var http = require('http');
 var colors = require('colors');
 var fs = require('fs');
-
 //cargando configuraciones
 var config = require('./config/config.js');
 var IP = config.IP;
 var PORT = config.PORT;
-
 // tema de colors
 //colors.setTheme(config.color_theme);
-
-var server = http.createServer(function(req, res){// codigo de nuestro server
-
-    res.writeHead(200,{
-        'Content-Type':'text/html'
+var server = http.createServer(function (req, res) {// codigo de nuestro server
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
     });
-
-fs.readFile('./static/index.html','utf8', 
-function(err, Content){
-    if(err){
-    console.log(`> Error al leer archivo: ${err}`);
-    res.writeHead(500,{
-        'Content-Type':'text/plain'
-    });
-    res.end("Error 500: Iternal Error...");
-}else{
-    //TODO: si sirve el archivo
-}
-
+    fs.readFile('./static/index.html', 'utf8',
+        function (err, Content) {
+            if (err) {
+                console.log(`> Error al leer archivo: ${err}`);
+                res.writeHead(500, {
+                    'Content-Type': 'text/plain'
+                });
+                res.end("Error 500: Iternal Error...");
+            } else {
+                //TODO: si sirve el archivo
+                console.log(">Se sirve el archivo: ./static/index.html ");
+                res.end(Content);
+            }
+      });
 });
-    
-});
-
-server.listen(PORT,IP, function(){
+server.listen(PORT, IP, function () {
     console.log(`> Server working @http://${IP}:${PORT}/`)
 });
