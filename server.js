4cd30535f8ed3dc2482613a8185895d0e4252bc4
Eduardo <er96099@gmail.com>
2016-11-08 08:51:10 -0600

    Servidor Estatico Modular con 404

--- server.js
+++ server.js
@@ -3,45 +3,23 @@ var colors = require('colors');
 var fs = require('fs');
 var mime = require('mime');
 var path = require('path');
+var staticServer = require('./Internals/static-server.js');
 
 //cargando configuraciones
 var config = require('./config/config.js');
 var IP = config.IP;
 var PORT = config.PORT;
 // tema de colors
-//colors.setTheme(config.color_theme);
+//color_theme.setTheme(config.color_theme);
 var server = http.createServer(function (req, res) {
     //extrayendo el path de la URL
     var urlpath = req.url;
-    // normalizando el path 
+    // normalizando el path en caso de no se pidea ningun recurso  
     if(urlpath==="/"){
-        urlpath =path.resolve("./static/index.html");
-    }else {
-        urlpath = path.resolve('./static'+urlpath);
+        urlpath =("/index.html");
     }
-    console.log(`>Recusrso Solicitado: ${urlpath}`);
-    // Decidiendo el conetent-Type enfuncion de la extencion del archivo solicitado
-    var mimeType = mime.lookup(urlpath);
-    
-    fs.readFile(urlpath,
-        function (err, Content) {
-            if (err) {
-                console.log(`> Error al leer archivo: ${err}`);
-                res.writeHead(500, {
-                    'Content-Type': 'text/plain'
-                });
-                res.end("Error 500: Iternal Error...");
-            } else {
-                //TODO: si sirve el archivo
-                res.writeHead(200,{
-                    'Content-Type':mimeType
-                });
-
-
-                console.log(`>Se sirve el archivo: ${path}`);
-                res.end(Content);
-            }
-      });
+        //se llama al servidor estatico
+        staticServer.serve(urlpath,res);
 });
 server.listen(PORT, IP, function () {
     console.log(`> Server working @http://${IP}:${PORT}/`)
